1852bd6f9baf3029573330d082049b4961c4bfc7
sagacity0321 <sagacity0321>
2023-08-30 00:00:47 +0000

    Pushed from Locofy Builder

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import {
+  Routes,
+  Route,
+  useNavigationType,
+  useLocation,
+} from "react-router-dom";
+import Index1 from "./pages/Index1";
+import LandingPage1 from "./pages/LandingPage1";
+import { useEffect } from "react";
+
+function App() {
+  const action = useNavigationType();
+  const location = useLocation();
+  const pathname = location.pathname;
+
+  useEffect(() => {
+    if (action !== "POP") {
+      window.scrollTo(0, 0);
+    }
+  }, [action, pathname]);
+
+  useEffect(() => {
+    let title = "";
+    let metaDescription = "";
+
+    switch (pathname) {
+      case "/":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/landing-page-1":
+        title = "";
+        metaDescription = "";
+        break;
+    }
+
+    if (title) {
+      document.title = title;
+    }
+
+    if (metaDescription) {
+      const metaDescriptionTag = document.querySelector(
+        'head > meta[name="description"]'
+      );
+      if (metaDescriptionTag) {
+        metaDescriptionTag.content = metaDescription;
+      }
+    }
+  }, [pathname]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Index1 />} />
+      <Route path="/landing-page-1" element={<LandingPage1 />} />
+    </Routes>
+  );
+}
+export default App;
---
+++ src/components/StyleOutlineSizeMBrandG.js
@@ -0,0 +1,25 @@
+import "./StyleOutlineSizeMBrandG.css";
+const StyleOutlineSizeMBrandG = ({
+  vector,
+  vector1,
+  vector2,
+  vector3,
+  vector4,
+  vector5,
+  vector6,
+}) => {
+  return (
+    <div className="styleoutline-sizem-brandg">
+      <img className="vector-icon5" alt="" src={vector} />
+      <img className="vector-icon5" alt="" src={vector1} />
+      <div className="get-it-on">GET IT ON</div>
+      <img className="vector-icon7" alt="" src={vector2} />
+      <img className="vector-icon8" alt="" src={vector3} />
+      <img className="vector-icon9" alt="" src={vector4} />
+      <img className="vector-icon10" alt="" src={vector5} />
+      <img className="vector-icon11" alt="" src={vector6} />
+    </div>
+  );
+};
+
+export default StyleOutlineSizeMBrandG;
